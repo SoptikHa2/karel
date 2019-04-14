6ceda39cbbfd067d05b9ccfb0ce3de1d368b3619
Petr Stastny <petr.stastny01@gmail.com>
2019-04-14 16:22:18 +0200

    Attempted to use code from multiple files

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io::stdin;
+use std::io::Read;
+use std::error::Error;
+
+use crate::core;
+
+/// Run with selected parameters. Use stdin and stdout for communication.
+pub fn run(interactive: bool, source: Option<&str>, libraries: Option<Vec<&str>>, ignore_runtime_errors: bool) {
+
+}
+
+fn load_from_file_or_stdin(filename_or_slash: &str) -> Result<String, Box<dyn Error>> {
+    if filename_or_slash == "-" {
+        // Load from stdin
+        let mut buffer: Vec<u8> = Vec::new();
+        stdin().read_to_end(&mut buffer)?;
+        return Ok(String::from_utf8(buffer)?);
+    }else{
+        // Load from file
+        return Ok(fs::read_to_string(&filename_or_slash)?);
+    }
+}
+
+fn run_interactive(libraries: Vec<&str>) {
+
+}
+
+fn run_from_source(source: &str, libraries: Vec<&str>) {
+
+}
+
+fn print_state(){
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-mod karel;
-
 extern crate clap;
 use clap::{App, Arg};
+
+mod lib;
+mod syntax;
+mod core;
 
 fn main() {
     let matches = App::new("Karel")
@@ -9,34 +11,33 @@ fn main() {
                     .author("Petr Šťastný <petr.stastny01@gmail.com>")
                     .about("Karel programming language interpreter")
                     .arg(Arg::with_name("interactive")
+                        .long("interactive")
                         .short("i")
                         .multiple(false)
                         .help("Start interactive session. This is not designed to communicate with other programs. User cannot create new methods, they have to be loaded with --lib.")
                         .required_unless("file")
                         .conflicts_with("file")
                         .takes_value(false))
-                    .arg(Arg::with_name("lib")
-                        .multiple(true)
-                        .help("Loaded library source code from file(s). Library source can only contain method definitions. Library cannot contain main.")
-                        .takes_value(true))
+                     .arg(Arg::with_name("ignore")
+                        .long("ignore")
+                        .multiple(false)
+                        .help("Ignore runtime errors in program. Program will not halt, it will skip runtime errors.")
+                        .takes_value(false))
                     .arg(Arg::with_name("file")
-                        .short("f")
                         .multiple(false)
                         .takes_value(true)
                         .help("Specify source file. The file has to contain main. You can add additional files as libraries.")
                         .required_unless("interactive")
-                        .conflicts_with("interactive"));
+                        .conflicts_with("interactive"))
+                    .arg(Arg::with_name("lib")
+                        .multiple(true)
+                        .help("Loaded library source code from file(s). Library source can only contain method definitions. Library cannot contain main.")
+                        .takes_value(true));
     let matches = matches.get_matches();
 
-    println!("Interacive: {}", matches.is_present("interactive"));
-    println!(
-        "Source file: {}",
-        matches.value_of("file").unwrap_or_else(|| "(None)")
-    );
-    println!(
-        "Libraries: {:?}",
-        matches
-            .values_of("lib")
-            .unwrap_or_else(|| clap::Values::default())
-    );
+    lib::run(
+        matches.is_present("interactive"),
+        matches.value_of("file"),
+        matches.values_of("lib").map(|v| v.collect()),
+        matches.is_present("ignore"));
 }
